65dd60f7611a297ebe545f896561c24147cfde7b
Yuri Isbener <yurisuhett@gmail.com>
2020-11-11 15:51:54 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Logica_de_programação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUm;
+            int valorDois;
+
+            // Aqui vai o código
+
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("-----------------------");
+            Console.Write("Digite o primeiro valor: ");
+            valorUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            valorDois = int.Parse(Console.ReadLine());
+
+            //Processamento
+            int soma = valorUm + valorDois;
+
+            //Exibir
+
+            Console.WriteLine("A Soma de "+valorUm+" + "+valorDois+" = "+soma);
+
+        }
+    }
+}
